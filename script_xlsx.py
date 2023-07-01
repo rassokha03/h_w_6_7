9c885b5036eb097b37ad42f6d26355af76e88740
Andrey Rassokhin <136015035+rassokha03@users.noreply.github.com>
2023-07-01 22:41:25 +0300

    Delete script_xlsx.py

--- script_xlsx.py
+++
@@ -1,6 +0,0 @@
-from openpyxl import load_workbook
-
-workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
-print(workbook.sheetnames)
-sheet = workbook.active
-print(sheet.cell(row=3, column=2).value)
